413357076deca5510d788d49ec22cbe21cabd81a
Eamonn Sullivan <eamonn.sullivan@bbc.co.uk>
2022-07-21 13:45:46 +0100

    Playing with errors.

--- greetings.go
+++ greetings.go
@@ -1,10 +1,18 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Hello returns a greeting for the named person.
-func Hello(name string) string {
-	// Return a greeting that embeds the name in a message.
-	message := fmt.Sprintf("Hi there, %v. Welcome!", name)
-	return message
+func Hello(name string) (string, error) {
+	var err error
+	var message string
+	if name == "" {
+		err = errors.New("Empty name!")
+	} else {
+		message = fmt.Sprintf("Hi, %v. Welcome!", name)
+	}
+	return message, err
 }
